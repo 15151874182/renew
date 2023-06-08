475515c939ff783c677cd11cb3503a0a3551e90a
cty <cty>
2023-06-08 14:54:52 +0800

    u

--- renew_center/dataclean/renew_clean.py
+++ renew_center/dataclean/renew_clean.py
@@ -11,7 +11,8 @@ import logging
 import warnings
 warnings.filterwarnings('ignore') 
 
-from renew_center.tools import setup_logger
+from datetime import date, timedelta
+from renew_center.tools.logger import setup_logger
 logger=setup_logger('logger')
 
 class Clean():
@@ -23,8 +24,8 @@ class Clean():
               use_type,  ##'train'(may delete bad feas)/'predcit'(fix data only)
               capacity,
               online,   ##None for wind clean
-              longitude, ##None for wind clean
-              latitude): ##None for wind clean
+              Longitude, ##None for wind clean
+              Latitude): ##None for wind clean
 
 #preparing ====================================================================        
         day_point=96  ##15min time freq everyday
@@ -32,21 +33,25 @@ class Clean():
         df=df[selected_feas]
         if area_type=='pv' and online==True:
             from astral.sun import sun
-            from astral import LocationInfo            
+            from astral import LocationInfo     
+            self.sun=sun
+            self.LocationInfo=LocationInfo            
             #suntime是日出日落信息，需要联网
             self.suntime = self.download_suntime(area=area,
                                                 belong='china',
-                                                longitude=longitude,
-                                                latitude=latitude,
+                                                Longitude=Longitude,
+                                                Latitude=Latitude,
                                                 begin="2018-1-1",
                                                 end="2024-12-31")
-            self.suntime["sunrise_before"] = self.suntime["Sunrise"].apply(lambda x:self.sunrise_before(x))
-            self.suntime["sunset_after"] = self.suntime["Sunset"].apply(lambda x:self.sunset_after(x))
-            self.suntime["start_zero_flag"] = 0               
+            self.suntime["sunrise_before"] = self.suntime["Sunrise"].apply(lambda x:pd.to_datetime(x).round('15T'))
+            self.suntime["sunset_after"] = self.suntime["Sunset"].apply(lambda x:pd.to_datetime(x).round('15T'))  
+            self.suntime.to_csv('data/suntime.csv')
+            
         elif area_type=='pv' and online==False:
             self.suntime = pd.read_csv('data/suntime.csv')
+            
 # =============================================================================             
-        for col in selected_feas:
+        for col in selected_feas: ##every feas should handle_NAN,handle_limit,handle_constant
             #### fea name should follow rules as below
             if "speed" in col:    ##wind speed
                 upper,lower = 35,0  
@@ -65,29 +70,29 @@ class Clean():
             else:
                 upper,lower=float('inf'),float('-inf')
             logging.info(f"{col},upper={upper},lower={lower}")   
-#1.handle_NAN =================================================================  
+            
+#handle_NAN =================================================================  
             df=self.handle_NAN(df,col,threshold=day_point//6)
-#2.handle_limit================================================================            
+#handle_limit================================================================            
             df=self.handle_limit(df,col,lower,upper)
+#special strategy for wind/pv================================================================             
             if area_type=='wind':
-                df=self.handle_constant(df,col,capacity,threshold=day_point//6)
-                
-            # if area_type=='wind' and use_type=='train':
-            #     pass
-            # elif area_type=='wind' and use_type=='predcit':
-            #     pass
-            # elif area_type=='pv' and use_type=='train':
-            #     pass
-            # elif area_type=='pv' and use_type=='predcit':
-            #     pass
+                pass  ##special strategy for wind if have will be put here 
+            elif area_type=='pv':
+                if ("rad" or 'load') in col:  ## fea related to suntime
+                    df=self.sunset_zero(df,col)
+#handle_constant================================================================        
+        ##constant=0 or capacity will be ignored,constant=0 problem will be put into similarity detect part             
+            df=self.handle_constant(df,col,capacity,threshold=day_point//6) 
+            xx=1
         
     def clean_area(self,config,selected_feas):
         return self.clean(data=config.data,
                           selected_feas=selected_feas,
                           use_type=config.use_type,  ##'train'/'predcit'
                           capacity=config.capacity,
-                          longitude=config.longitude,
-                          latitude=config.latitude)
+                          Longitude=config.Longitude,
+                          Latitude=config.Latitude)
     
     def check_length(self,data,day_point,threshold):
         days=len(data)//day_point
@@ -97,7 +102,7 @@ class Clean():
     def handle_NAN(self,data,col,threshold):
         df=deepcopy(data)
         #### spline interpolate if continues num of NAN is within threshold
-        df[col].interpolate(method='spline', limit=threshold,axis=0)
+        df[col].interpolate(method='cubic', limit=threshold,axis=0)
         return df
 
     def handle_limit(self,data,col,lower,upper):
@@ -107,12 +112,16 @@ class Clean():
         return df
 
     def handle_constant(self,data,col,capacity,threshold):
+        '''
+        delete threshold points constant data[col] except constant=0 or capacity
+        threshold->int, how many continues points considered
+        '''
         df=deepcopy(data)
         #### continues constant array's std must be zero
         df['constant_std'] = df[col].rolling(window=threshold).std()
         df['constant_mu'] = df[col].rolling(window=threshold).mean()
         df['index']=range(len(df)) ##data's index are date so....
-        condition=df['constant_std']==0 and (df['constant_mu']!=0 or df['constant_mu']!=capacity)
+        condition=(df['constant_std']==0) & ((df['constant_mu']!=0) | (df['constant_mu']!=capacity))
         constant_end_index=df[condition]['index'] ##find end index
         constant_index=[] ##find all index not wanted
         for i in constant_end_index:
@@ -122,7 +131,7 @@ class Clean():
         del df['index'],df['constant_std'],df['constant_mu'] ##delete auxiliary varible
         return df
 
-    def download_suntime(self,area,belong,longitude,latitude,begin,end):
+    def download_suntime(self,area,belong,Longitude,Latitude,begin,end):
         """
         根据输入的经度、维度，下载从起始到结束之间的每天日出日落时间
         该函数使用的时候需要联网环境
@@ -149,7 +158,7 @@ class Clean():
     
         #example:    
         # city = LocationInfo("Nanjing", "China", "China/Nanjing", latitude, longitude)#纬度，经度
-        city = self.LocationInfo(area, belong, f"{belong}/{area}", latitude, longitude)#纬度，经度
+        city = self.LocationInfo(area, belong, f"{belong}/{area}", Latitude, Longitude)#纬度，经度
         logging.info((
             f"Information for {city.name}/{city.region}\n"
             f"Timezone: {city.timezone}\n"
@@ -185,120 +194,23 @@ class Clean():
     
         return df    
 
-
-    def sunrise_before(self,x):
-        """
-        找到日出前的一个15分钟间隔的点
-        """
-        x = pd.to_datetime(x)
-        #取分钟数据，并转换成int
-        time = x.strftime("%Y-%m-%d %H")
-        M = int(x.strftime("%M"))
-        if M<=15:
-            M=0
-        elif M<=30:
-            M=15
-        elif M<=45:
-            M=30
-        elif M<=60:
-            M=45
-        time = time+f":{M}"
-        return time
-
-    def sunset_after(self,x):
-        """
-        找到日落后的第一个15分钟间隔的点
-        """
-        x = pd.to_datetime(x)
-        #取分钟数据，并转换成int
-        time = x.strftime("%Y-%m-%d ")
-        M = int(x.strftime("%M"))
-        H = int(x.strftime("%H"))
-        if M<=15:
-            M=15
-        elif M<=30:
-            M=45
-        elif M<=45:
-            M=0
-            H = H+1
-        elif M<=60:
-            M=0
-            H = H+1
-        time = time+f"{H}:{M}"
-        return time
-            
-    def sunset_zero(self,data,cols):
-        """
-        函数功能：将日落时间段， 
-        辐照度和负荷列["GlobalR","DirectR","DiffuseR","load"]
-        强制设置为0
-        """
-        
-        #将suntime数据中指定的时间信息列，转换成时间类型
-        sun_date_cols = ["Sunrise","Sunset","sunrise_before","sunset_after"]
-        for col in sun_date_cols:
-            self.suntime[col] = pd.to_datetime(self.suntime[col])
-        
-        df = copy.deepcopy(data)
-        #读取原始数据的列名，用于返回函数处理结果时返回一模一样的列
-        ori_col = df.columns.tolist()
-        ori_col.append("sunset_flag")
-        
-        #为df创建一天为单位的时间列dayofdate
-        df["dayofdate"] = df["date"].apply(lambda x:x.strftime("%Y-%m-%d"))
-        
-        #处理日出前的置零工作
-        sunrise_time = self.suntime.loc[:,["sunrise_before","start_zero_flag"]]
-        df = pd.merge(left =df,right= sunrise_time,left_on="date",right_on="sunrise_before",how="left")
-        df = df.reset_index(drop=True)
-    
-        #按天进行分组，然后进行start_zero_flag向后填充
-        def _bfill_flag(x):
-            #每天日出前的flag填充为0
-            x["start_zero_flag"].fillna(method="bfill",inplace=True)
-            #将剩下的点填充为1
-            x["start_zero_flag"].fillna(1,inplace=True)
-            return x
-        grouped = df.groupby("dayofdate")
-        df = grouped.apply(_bfill_flag)
-    
-        #筛选出日落的index，将相应的属性列在置零
-        sunset_index = df[df["start_zero_flag"]==0].index.tolist()
-        for col in cols:
-            df.loc[sunset_index,col] = 0
-        #创建一列sunset_flag，来标记日落的时间段。日落=1，其他=0
-        df["sunset_flag"] = 0
-        df.loc[sunset_index,"sunset_flag"] = 1
-        
-        #选择df原有的列，接着继续进行日落后数据清零的工作
-        df = df.loc[:,ori_col]
-        
-        #为df创建一天为单位的时间列dayofdate
-        df["dayofdate"] = df["date"].apply(lambda x:x.strftime("%Y-%m-%d"))
-        #处理日落后的置零工作
-        sunset_time = self.suntime.loc[:,["sunset_after","start_zero_flag"]]
-        df = pd.merge(left =df,right= sunset_time,left_on="date",right_on="sunset_after",how="left")
-        df = df.reset_index(drop=True)
-    
-        #按天进行分组，然后进行start_zero_flag向后填充
-        def _ffill_flag(x):
-            #每天日出前的flag填充为0
-            x["start_zero_flag"].fillna(method="ffill",inplace=True)
-            #将剩下的点填充为1
-            x["start_zero_flag"].fillna(1,inplace=True)
-            return x
-        grouped = df.groupby("dayofdate")
-        df = grouped.apply(_ffill_flag)
-    
-        #筛选出日落的index，将相应的属性列在置零
-        sunset_index = df[df["start_zero_flag"]==0].index.tolist()
-        for col in cols:
-            df.loc[sunset_index,col] = 0
-        #标记日落的时间段。日落=1，其他=0
-        df.loc[sunset_index,"sunset_flag"] = 1
-        
-        #选择df原有的列，并返回处理后的数据
-        df = df.loc[:,ori_col]
-        
+    def sunset_zero(self,data,col):
+        df=deepcopy(data)
+        sunset_point=pd.DataFrame(sorted(pd.concat([self.suntime['sunrise_before'],self.suntime['sunset_after']]).drop_duplicates()))
+        sunset_point.columns=['sunset_point']
+        sunset_point.index=pd.to_datetime(sunset_point['sunset_point']) ##change index into datetime
+        df=df.join(sunset_point)
+        sunset_list=list(df[~df['sunset_point'].isna()]['sunset_point']) ##sunset Separation point list
+        res=[] ##full sunset point list
+        for idx,time in enumerate(sunset_list):
+            if idx==0 and time.hour<12: ##create first point sunset points 
+                res+=list(pd.date_range(time-pd.Timedelta(8,unit='H'),time,freq='15T'))
+            elif idx==len(sunset_list)-1 and time.hour>12: ##create last point sunset points 
+                res+=list(pd.date_range(time,time+pd.Timedelta(8,unit='H'),freq='15T'))
+            elif time.hour>12:  ##create middle sunset points 
+                res+=list(pd.date_range(sunset_list[idx],sunset_list[idx+1],freq='15T'))
+        df['date']=df.index
+        df['sunset_flag']=df['date'].apply(lambda x:True if x in res else False)
+        df[col][df['sunset_flag']==True]=0 ##sunset points set to zero
+        del df['sunset_flag'],df['date'],df['sunset_point']
         return df
-
